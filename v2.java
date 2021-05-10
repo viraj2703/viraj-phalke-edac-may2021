2b8235155e474c1fd120ae82be8e32d143c790f0
viraj2703 <83832340+viraj2703@users.noreply.github.com>
2021-05-10 22:17:09 +0530

    Add files via upload

---
+++ v2.java
@@ -0,0 +1,7 @@
+public class v2
+{
+public static void main(String[] args)
+{
+System.out.println(23+15);
+}
+}
---
+++ v9.java
@@ -0,0 +1,9 @@
+
+
+public class v9
+{
+    public static void main(String args[])
+{
+    System.out.println("Output is :"+((25.5 * 3.5) - (3.5 * 3.5)) / (40.5 - 4.5));
+}
+}
